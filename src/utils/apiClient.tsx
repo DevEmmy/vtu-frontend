28cc12c940378c4306c16e0bc14688bd1a86cba5
DevEmmy <eolaosebikan60@gmail.com>
2024-08-22 16:02:30 +0100

    updated api url

--- src/utils/apiClient.tsx
+++ src/utils/apiClient.tsx
@@ -5,7 +5,7 @@ const getToken = ()=>{
 }
 
 export const axiosConfig = axios.create({
-    baseURL: "http://192.168.236.168:4030",
+    baseURL: "https://megapay-backend.onrender.com",
     headers: {
         "Authorization": `Bearer ${getToken()}`
     }
