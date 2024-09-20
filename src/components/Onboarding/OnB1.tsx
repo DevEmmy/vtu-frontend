b0f3935f3b3839e9918e68375f934c69cfed1239
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-09-20 14:05:19 +0100

    Updates: skip button activated on On-boarding page.

--- src/components/Onboarding/OnB1.tsx
+++ src/components/Onboarding/OnB1.tsx
@@ -1,35 +1,39 @@
-
+import { Link } from "react-router-dom";
 
 interface Props {
-    next: () => void
+  next: () => void;
 }
 
 const OnB1 = ({ next }: Props) => {
-    return (
-        <div className='flex flex-col pb-3'>
-            <img src="./ob1.png" className='w-full object-contain' alt="" />
+  return (
+    <div className="flex flex-col pb-3">
+      <img src="./ob1.png" className="w-full object-contain" alt="" />
 
-            <div className="steps">
-                <div className="line !bg-white !opacity-100" />
-                <div className="line" />
-                <div className="line" />
-            </div>
+      <div className="steps">
+        <div className="line !bg-white !opacity-100" />
+        <div className="line" />
+        <div className="line" />
+      </div>
 
-            <div className="onboarding_container">
+      <div className="onboarding_container">
+        <p className="text-[22px] font-bold">Welcome to VTU!</p>
+        <p className="text-sm">
+          Hi there! We're excited to have you on board. VTU is your one-stop app
+          for all your payment needs.
+        </p>
 
-                <p className='text-[22px] font-bold'>Welcome to VTU!</p>
-                <p className='text-sm'>
-                    Hi there! We're excited to have you on board. VTU is your one-stop app for all your payment needs.
-                </p>
+        <button
+          className=" bg-primary text-white font-semibold py-2 px-10 rounded-full"
+          onClick={next}
+        >
+          Next
+        </button>
+        <Link to={"/new-account"}>
+          <p className="text-xs">skip</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-                <button className=' bg-primary text-white font-semibold py-2 px-10 rounded-full' onClick={next}>
-                    Next
-                </button>
-
-                <p className='text-xs'>skip</p>
-            </div>
-        </div>
-    )
-}
-
-export default OnB1
+export default OnB1;
--- src/components/Onboarding/OnB2.tsx
+++ src/components/Onboarding/OnB2.tsx
@@ -1,35 +1,39 @@
+import { Link } from "react-router-dom";
 
-
-interface Props{
-    next: ()=> void
+interface Props {
+  next: () => void;
 }
 
-const OnB2 = ({next}: Props) => {
-    return (
-        <div className='pb-3 flex flex-col '>
-        <img src="./ob2.png" className='w-full mx-auto object-contain' alt="" />
+const OnB2 = ({ next }: Props) => {
+  return (
+    <div className="pb-3 flex flex-col ">
+      <img src="./ob2.png" className="w-full mx-auto object-contain" alt="" />
 
-        <div className="steps">
-            <div className="line !bg-white !opacity-100" />
-            <div className="line !bg-white !opacity-100" />
-            <div className="line" />
-        </div>
+      <div className="steps">
+        <div className="line !bg-white !opacity-100" />
+        <div className="line !bg-white !opacity-100" />
+        <div className="line" />
+      </div>
 
-        <div className="onboarding_container">
+      <div className="onboarding_container">
+        <p className="text-[22px] font-bold">Explore Key Features</p>
+        <p className="text-sm">
+          Buy data bundles and airtime for any network, Pay electricity and
+          other utility bills, services like GOtv.
+        </p>
 
-            <p className='text-[22px] font-bold'>Explore Key Features</p>
-            <p className='text-sm'>
-            Buy data bundles and airtime for any network, Pay electricity and other utility bills, services like GOtv.
-            </p>
-
-            <button className=' bg-primary text-white font-semibold py-2 px-10 rounded-full' onClick={next}>
-                Next
-            </button>
-
-            <p className='text-xs'>skip</p>
-        </div>
+        <button
+          className=" bg-primary text-white font-semibold py-2 px-10 rounded-full"
+          onClick={next}
+        >
+          Next
+        </button>
+        <Link to={"/new-account"}>
+          <p className="text-xs">skip</p>
+        </Link>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default OnB2
+export default OnB2;
