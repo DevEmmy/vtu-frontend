a7a5dae1b14730d73b7f057d9d21d2422669e400
Danny Uzo <uzodinmadaniel42@gmail.com>
2024-09-20 13:56:56 +0100

    Updates: new toast added; toast.info

--- src/utils/toast.tsx
+++ src/utils/toast.tsx
@@ -28,3 +28,17 @@ export const toastError = (message: string) => {
         transition: Bounce,
     });
 }
+
+export const toastInfo = (message: string) => {
+    toast.info(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+    });
+}
