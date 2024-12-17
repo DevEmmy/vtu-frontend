4ae10ce9e08f2ad355d629c657265fa3dfc2a245
DevEmmy <eolaosebikan60@gmail.com>
2024-12-17 15:54:53 +0100

    updated payment service provider to paystack

--- src/components/home/quick-actions/fund-wallet.tsx
+++ src/components/home/quick-actions/fund-wallet.tsx
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 import  useMonnify  from "../../../hooks/useMonnify";
 import { useState } from "react";
 import { useFundWallet, useUser } from "../../../hooks/Auth";
-
+import { PaystackButton } from "react-paystack";
+import { v4 as uuidv4 } from 'uuid';
 
 function fundWallet() {
   const [amount, setAmount] = useState(0)
@@ -14,32 +15,40 @@ function fundWallet() {
 
   const {user} = useUser();
 
+
+
   const config = {
-    amount: amount,
-    currency: 'NGN',
-    reference: `${new String(new Date().getTime())}`,
-    customerName: `${user.firstName} ${user.lastName}`,
-    customerEmail: `${user.email}`,
-    paymentDescription: 'Funding wallet',
-    metadata: {
-      name: `${user.firstName} ${user.lastName}`,
-    },
-    isTestMode: false,
-    customerPhoneNumber: `${user.phoneNumber}`,
-    apiKey: import.meta.env.VITE_MONNIFY_API_KEY as string,
-    contractCode: import.meta.env.VITE_CONTRACT_CODE as string,
+    reference: uuidv4(),
+    email: user.email,
+    amount: amount * 100,
+    publicKey: "sk_test_de08c04eb8b47c95d24fc8383fdcae573dbdb996",
   };
-  const {startPayment} = useMonnify(config)
+
+  const handlePaystackSuccessAction = (reference) => {
+    // Implementation for whatever you want to do with reference and after success call.
+    
+    let data = {
+      email : user.ammount,
+      name : user.firstName + user.lastName,
+      amount: config.amount / 100,
+      ref: reference.reference,
+    }
+    console.log(data);
+    // createTransaction(data)
+  };
+
   
-  const submit = ()=>{
-   startPayment(
-      () => {console.log('loading has started'); fundWalletFn({amount})},
-      () => console.log('SDK is UP'),
-      (res) =>{ console.log('response', res); fundWalletFn({amount})},
-      (data) => console.log('data', data)
-    )
+  const handlePaystackCloseAction = () => {
+    // implementation for  whatever you want to do when the Paystack dialog closed.
+    console.log('closed')
   }
 
+  const componentProps = {
+    ...config,
+    text: 'Proceed to Payment',
+    onSuccess: (reference) => handlePaystackSuccessAction(reference),
+    onClose: handlePaystackCloseAction,
+};
   
   return (
     <div className='px-3 py-5 flex flex-col gap-7 min-h-screen'>
@@ -50,7 +59,8 @@ function fundWallet() {
         <div className="flex flex-col gap-5">
           <p>How Much do you want to fund?</p>
           <input type="number" className="border-b focus:outline-none p-3" placeholder="100 - 100,000" onChange={(e: any)=> setAmount(e.target.value)}/>
-          <button className="bg-primary rounded-lg p-4 text-white" onClick={submit}>Proceed to Payment</button>
+          {/* <button className="bg-primary rounded-lg p-4 text-white" onClick={submit}>Proceed to Payment</button> */}
+          <PaystackButton {...componentProps} className='border-primary1 border-2 py-3 px-16 md:w-full rounded-md text-white my-5  hover:text-white transition-all bg-primary' />
         </div>
     </div>
   )
--- src/utils/apiClient.tsx
+++ src/utils/apiClient.tsx
@@ -3,9 +3,11 @@ import axios from "axios";
 const getToken = ()=>{
     return localStorage.getItem("token")
 }
-"https://megapay-backend.onrender.com"
+
+// "https://megapay-backend.onrender.com"
+
 export const axiosConfig = axios.create({
-    baseURL: "http://192.168.87.168:4030",
+    baseURL: "http://192.168.223.168:4030",
     headers: {
         "Authorization": `Bearer ${getToken()}`
     }
